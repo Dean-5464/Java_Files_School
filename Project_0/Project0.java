8d8ad00c10f497ca4de15fc94c86fa47b0dfe225
Dean-5464 <87671890+Dean-5464@users.noreply.github.com>
2021-07-22 10:54:19 -0500

    Add files via upload

---
+++ Project_0/Project0.java
@@ -0,0 +1,18 @@
+
+/**
+ * The Hello World Program Used To Learn How To Use And Configure BlueJ
+ *
+ * Author: Dean Kammerer
+ * Date: 8/26/2020
+ */
+public class Project0
+{
+    
+    //main method
+    public static void main(String[] args)
+    {
+    //displays the string "Hello, World!" to the console and terminates the code
+    System.out.print("Hello, World!");
+    }
+
+}
